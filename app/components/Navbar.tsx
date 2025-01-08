f01a5b6e934c55a39d38c564eba4c70a4a772615
ChandraShekhar <csyadav0513@gmail.com>
2025-01-08 21:12:46 +0530

    Add navbar github logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
-        <Image
-          src={"https://img.icons8.com/3d-fluency/94/github.png"}
-          alt="Github"
-          width={20}
-          height={20}
-        />
+    <div className="w-full mt-5 fixed z-50 ">
+      <div className="absolute right-5 md:right-10">
+        <Link href={"https://github.com/StarDust130"} target="_blank">
+          <Image
+            src={"https://img.icons8.com/3d-fluency/94/github.png"}
+            alt="Github"
+            width={30}
+            height={30}
+          />
+        </Link>
       </div>
     </div>
   );
 };
+
 export default Navbar;
