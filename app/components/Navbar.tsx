3ea292e15960255487ea9664849b55b4a337075e
ChandraShekhar <csyadav0513@gmail.com>
2025-01-18 13:05:16 +0530

    refactor Navbar component for improved layout and styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="w-full mt-5 fixed z-50 ">
-      <div className="absolute right-5 md:right-10">
+    <div className="w-full  my-3 flex items-center justify-between px-5 md:px-10">
+      <div className="flex justify-end w-full">
         <Link href={"https://github.com/StarDust130"} target="_blank">
           <Image
             src={"https://img.icons8.com/3d-fluency/94/github.png"}
