0aa9fbd859cca1e1acfad26b2901053de27166c1
ChandraShekhar <csyadav0513@gmail.com>
2025-01-20 20:19:01 +0530

    remove AppleCardsCarousel component and clean up About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,10 @@
-import { AppleCardsCarousel } from "../components/AppleStyleCards";
 import { TimelineUI } from "../components/Timeline";
 
 const About = () => {
   return (
     <div className="flex flex-col w-full h-screen">
       <TimelineUI />
-      <AppleCardsCarousel />
+      
     </div>
   );
 };
