a4718bfc530158ea5e2544ccc809f0c67b0ef024
ChandraShekhar <csyadav0513@gmail.com>
2025-01-20 19:30:36 +0530

    add BgSvg component and integrate it into the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,17 @@
-import Link from "next/link";
+import { TimelineUI } from "./components/Timeline";
+import { AppleCardsCarousel } from "./components/AppleStyleCards";
+import Hero from "./components/Hero";
+import BgSvg from "./components/BgSvg";
 
 const Page = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white flex flex-col items-center justify-center font-inter relative">
-      {/* Background SVGs */}
-      <svg
-        className="absolute top-10 left-1/4 sm:left-32 w-24 h-24 opacity-20"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 50 Q50 30, 100 50"
-          stroke="rgba(255, 255, 255, 0.3)"
-          strokeWidth="2"
-          fill="none"
-        />
-      </svg>
-      <svg
-        className="absolute bottom-10 left-1/2 sm:left-60 w-24 h-24 opacity-15"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 60 C40 80, 60 20, 100 60"
-          stroke="rgba(255, 255, 255, 0.4)"
-          strokeWidth="2"
-          fill="none"
-        />
-      </svg>
-      <svg
-        className="absolute top-20 right-10 w-24 h-24 opacity-25"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 50 C50 30, 50 70, 100 50"
-          stroke="rgba(255, 255, 255, 0.5)"
-          strokeWidth="2"
-          fill="none"
-        />
-      </svg>
-      <svg
-        className="absolute bottom-60 right-1/4 sm:right-36 w-12 h-12 opacity-25"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 50 C50 30, 50 70, 100 50"
-          stroke="rgba(255, 255, 255, 0.5)"
-          strokeWidth="2"
-          fill="none"
-        />
-      </svg>
+    <div className="w-full  md:h-screen  flex flex-col  font-inter relative">
+   <BgSvg />
 
-      {/* Content Section */}
-      <div className="container mx-auto px-4 sm:px-8">
-        {/* Header Section */}
-        <div className="text-center space-y-6 md:max-w-3xl mx-3 md:mx-auto">
-          <h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-blue-500 to-purple-600">
-            Hi, I&apos;m Chandrashekhar!
-          </h1>
-        </div>
+      <Hero />
 
-        {/* About Me Section */}
-        <div className="mt-6 md:mt-12 text-center md:max-w-3xl mx-3 md:mx-auto">
-          <p className="text-base sm:text-lg lg:text-xl text-gray-400">
-            Passionate about tech 💻, reading 📚, anime, chess ♟️, geopolitics
-            🌍, and philosophy 🤔. Believes in constant learning and growth. 🔥
-          </p>
-        </div>
-
-        <div className="flex  items-center justify-center gap-6 mt-6">
-          <Link href="/about">
-            <button className="button mt-3">
-              <div>
-                <span>Discover Now</span>
-              </div>
-            </button>
-          </Link>
-        </div>
-      </div>
+      <AppleCardsCarousel />
+      <TimelineUI />
     </div>
   );
 };
