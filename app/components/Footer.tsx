374cc6dbd3e200de5db98e07b9841bf6f150026f
ChandraShekhar <csyadav0513@gmail.com>
2025-02-05 11:43:11 +0530

    style(footer): add rounded corners to footer for enhanced aesthetics

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="py-10  text-white border-t border-gray-700">
+    <footer className="py-10 rounded-t-3xl
+      text-white border-t border-gray-700">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="flex justify-center sm:justify-start">
