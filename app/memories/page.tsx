6fa2b733a456288d211c5663f87030c09ccc555e
ChandraShekhar <csyadav0513@gmail.com>
2025-02-07 13:08:12 +0530

    feat(memories): update Memories component styling for improved layout

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -3,7 +3,7 @@ import MemoriesPage from "../components/MemoriesPage";
 
 const Memories = () => {
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col w-full min-h-screen bg-black text-white">
       <MemoriesPage />
     </div>
   );
