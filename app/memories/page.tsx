3d23bdb6025eead151387cae4d968baa10d3c285
ChandraShekhar <csyadav0513@gmail.com>
2025-02-07 12:13:33 +0530

    feat(memories): add Memories component with layout for MemoriesPage

---
+++ app/memories/page.tsx
@@ -0,0 +1,11 @@
+import MemoriesPage from "../components/MemoriesPage";
+
+
+const Memories = () => {
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <MemoriesPage />
+    </div>
+  );
+};
+export default Memories;
