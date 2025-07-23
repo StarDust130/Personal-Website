82d76f8f54139f336fe416b8b0f37d3c3fd12b2b
ChandraShekhar <csyadav0513@gmail.com>
2025-07-23 23:25:09 +0530

    done

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
-import MemoriesPage from "../components/MemoriesPage";
+// import MemoriesPage from "../components/MemoriesPage";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Memories - Chandrashekhar",
@@ -27,8 +28,20 @@ export const metadata: Metadata = {
 
 const Memories = () => {
   return (
-    <div className="flex flex-col w-full min-h-screen bg-black text-white">
-      <MemoriesPage />
+    // <div className="flex flex-col w-full min-h-screen bg-black text-white">
+    //   {/* <MemoriesPage /> */}
+    // </div>
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-black text-white space-y-6">
+      <h1 className="text-3xl md:text-5xl font-bold text-center">
+        📚 Study Hard, LEVEL 7 Baddie 😉💙
+      </h1>
+      <Image
+        src={"https://c.tenor.com/7fzWR4STioAAAAAC/tenor.gif"}
+        alt="Study Hard Baddie"
+        height={500}
+        width={500}
+        className="rounded-2xl shadow-lg"
+      />
     </div>
   );
 };
